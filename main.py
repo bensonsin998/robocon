5cecca2d593897c87748dcd109b40f9622893ba2
bensonsin998 <56331577+bensonsin998@users.noreply.github.com>
2020-05-04 14:44:20 +0800

    git commit

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import cv2
 import numpy as np
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cam.read()
+    vis = img.copy()
+    cvs.imshow("Cmaera", vis)
+    if cv2.waitKey(5) == 27:
+      break;
+
+cv2.destroyAllWindows()
