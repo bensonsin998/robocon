7eb6383bf458e24e06f236611ac74f4f156016e1
bensonsin998 <56331577+bensonsin998@users.noreply.github.com>
2020-05-04 15:18:15 +0800

    git commit

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import cv2
 import numpy as np
 
-cam = cv2.VideoCapture(0)
+process = True
+quitbutton = 27     #Esc
 
-while True:
-    screen = cam.read()
-    cv2.imshow("Camera", screen)
-    if cv2.waitKey(5) == 'q':
-      break;
+cam = cv2.VideoCapture(0)
+if not cam.isOpened():
+    print("No camera connected")
+    exit(0)
+
+while process:
+    ret, screen = cam.read()
+    display = screen.copy()
+    cv2.imshow("Camera", display)
+
+    if(0xFF & cv2.waitKey(1) == quitbutton):
+        print("Esc is pressed")
+        process = False
 
 cv2.destroyAllWindows()
