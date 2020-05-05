a4202a7999e21ffcb120277a62430c82ef64bf85
bensonsin998 <56331577+bensonsin998@users.noreply.github.com>
2020-05-05 23:50:12 +0800

    git commit

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 #               2. Read the frame from the camera
 #               3. Locate the object: rugby ball
 #               4. Keep Calculating the position between the camera and the object
-from collections import deque
 import cv2 as cv
 import imutils
 import numpy as np
@@ -14,20 +13,19 @@ if not cam.isOpened():      #Error handler -> When cannot open the camera
     print("Error: Cannot open camera!!!")
     exit();
 else:
-    cam_Width = cam.get(cv.CAP_PROP_FRAME_WIDTH)
-    cam_Height = cam.get(cv.CAP_PROP_FRAME_HEIGHT)
-    cam_Width_Middle = cam_Width / 2
+    window_width = cam.get(cv.CAP_PROP_FRAME_WIDTH)
+    window_Height = cam.get(cv.CAP_PROP_FRAME_HEIGHT)
+    window_width_Middle = window_width / 2
+    window_margin = 100
 escButton = 27 #ESC
 #Target Object (rugby ball)
 center = None
 contours = None
-direction = None
 dx, dy = None, None
 HSVLower = (29, 86, 6)
 HSVUpper = (64, 255, 255)
-minSize = 10
-points = deque(maxlen = 100)
-points_counter = 0
+minSize = 20
+points = []
 position = None
 radius = None
 x, y = 0 , 0
@@ -48,10 +46,10 @@ while True:
   contours = cv.findContours(frame_mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
   contours = imutils.grab_contours(contours)
 
+  #TODO: Find the distance and position from the camera to the target
   #If the program find one or more contours
   if len(contours) > 0:
     larget_contours = max(contours, key = cv.contourArea)
-
     ((x, y), radius) = cv.minEnclosingCircle(larget_contours)
     mom = cv.moments(larget_contours)
     center = (int(mom["m10"] / mom["m00"]), int(mom["m01"] / mom["m00"]))
@@ -59,27 +57,30 @@ while True:
     if radius > minSize:   #The rball requires at least 10 pixel radius to track it
       cv.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2) #Draw the minimum enclosing circle around the rball
       cv.circle(frame, center, 5, (0, 0, 255) , -1)   #Draw the center of the rball
-      points.appendleft(center)                 #Update the list of points containing the center (x, y) of the object
+      points.append(center)                 #Update the list of points containing the center (x, y) of the object
 
-      #Testing
-      print("Center: ", center)
-      print("X, Y", (x, y))
+      if x < window_width_Middle - window_margin:
+        position = "Left"
 
-  #TODO: Find the distance and position from the camera to the target
-  if x < cam_Width_Middle - 100:
-    direction = "Right"
-  elif x >= cam_Width_Middle - 100 and x <= cam_Width_Middle + 100:
-    if x < cam_Width_Middle:
-      direction = "Middle - Right"
-    elif x == cam_Width_Middle:
-      direction = "Middle"
+      elif x >= window_width_Middle - window_margin and x <= window_width_Middle + window_margin:
+        if x < window_width_Middle:
+          position = "Middle - Left"
+
+        elif x == window_width_Middle:
+          position = "Middle"
+
+        else:
+          position = "Middle - Right"
+
+      else:
+        position = "Right"
+
     else:
-      direction = "Middle - Left"
-  else:
-    direction = "Left"
+      position = ""
 
-  cv.putText(frame, direction, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 3)
-  cv.putText(frame, "dx: {}, dy: {}".format(dx, dy), (10, 670), cv.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+  #Show the position and the moving direction(dx, dy) from the camera to the target on the screen
+  cv.putText(frame, "Position: {}".format(position), (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 3)
+  cv.putText(frame, "dx: {}, dy: {}".format(dx, dy), (10, int(window_Height) - 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
 
   #Testing
   cv.imshow("Frame_mask", frame_mask)
@@ -91,7 +92,6 @@ while True:
   if cv.waitKey(1) == escButton:
     break
 
-  points_counter += 1
-
 cam.release()
 cv.destroyAllWindows()
+print("End of Program")
