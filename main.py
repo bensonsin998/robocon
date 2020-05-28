8f8c018b03d1de5532612de824f08291b2992b70
bensonsin998 <56331577+bensonsin998@users.noreply.github.com>
2020-05-28 15:23:24 +0800

    master

--- main.py
+++ main.py
@@ -82,59 +82,74 @@ while True:
 
     ((x, y), radius) = cv.minEnclosingCircle(mixed_contour)
     mom = cv.moments(mixed_contour)
-    center = (int(mom["m10"] / mom["m00"]), int(mom["m01"] / mom["m00"]))
+    #center = (int(mom["m10"] / mom["m00"]), int(mom["m01"] / mom["m00"]))
 
     if radius >= min_size:
-      cv.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2) #Draw the minimum enclosing circle around the rball
-      cv.circle(frame, center, 5, (0, 0, 255) , -1)   #Draw the center of the rball
-      points.append(center)                 #Update the list of points containing the center (x, y) of the object
-
-      #Find object position
-      if x < window_left:   #Object is located in left region
-        position = "Left"
-        v_x = -1
-        v_y = 1
-        v_z = 0
-
-      elif x >= window_left and x < window_right: #Object is located in middle region
-        if x < window_mid_left:
-          position = "Middle - Left"
-
-        elif x >= window_mid_left and x < window_mid_right:
-          position = "Middle"
-
-        else:
-          position = "Middle - Right"
-
-        v_x = 0
-        v_y = 1
-        v_z = 0
-
-      else:               #Object is located in right region
-        position = "Right"
-        v_x = 1
-        v_y = 1
-        v_z = 0
-
-      distance = (object_width * focal_length / (radius * 2)) / 10
-      distance = round(distance, 3)
-
-      if distance <= 3.0:
-        if position == "Left" or position == "Middle - Left":
-          v_z = -1
-
-        elif position == "Right" or position == "Middle - Right":
-          v_z = 1
+      found = True
 
     else:
-      position = ""
-      distance = -1.0
-      v_x = 0
-      v_y = 0
+      found = False
+
+  else:
+    found = False
+
+  #If object is found, then circle the object, and calculate the distance and velocity
+  if found:
+    cv.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2) #Draw the minimum enclosing circle around the rball
+    cv.circle(frame, (int(x), int(y)), 5, (0, 0, 255) , -1)   #Draw the center of the rball
+
+    #Find object position
+    if x < window_left:   #Object is located in left region
+      position = "Left"
+      v_x = -1
+      v_y = 1
       v_z = 0
 
-  velocity = (v_x, v_y, v_z)
-  print(velocity)
+    elif x >= window_left and x < window_right: #Object is located in middle region
+      if x < window_mid_left:
+        position = "Middle - Left"
+
+      elif x >= window_mid_left and x < window_mid_right:
+        position = "Middle"
+
+      else:
+        position = "Middle - Right"
+
+      v_x = 0
+      v_y = 1
+      v_z = 0
+
+    else:               #Object is located in right region
+      position = "Right"
+      v_x = 1
+      v_y = 1
+      v_z = 0
+
+    #Find object distance to camera
+    distance = (object_width * focal_length / (radius * 2)) / 10
+    distance = round(distance, 3)
+
+    #Rotate the robot if the distance is less then 3cm
+    if distance <= 3.0:
+      if position == "Left" or position == "Middle - Left":
+        v_z = -1
+
+      elif position == "Right" or position == "Middle - Right":
+        v_z = 1
+
+      else:
+        v_z = 0
+
+    #Return velocity
+    velocity = (v_x, v_y, v_z)
+    print(velocity)     #Testing
+
+  #If object is not found, then reset variable
+  else:
+    position = ""
+    distance = -1.0
+    velocity = (0, 0, 0)
+    print(velocity)     #Testing
 
   #Show the position and the moving direction(dx, dy) from the camera to the target on the screen
   cv.putText(frame, "Position: {}".format(position), (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
